5b9aae42ee93bf0cbf6af424a987ffb502cf17f5
wilfredopacheco <wilfredo.pacheco.sa@gmail.com>
2018-11-08 20:45:01 -0600

    made it give me more errors

--- src/main/java/com/codeup/closetrycorner/controllers/UserController.java
+++ src/main/java/com/codeup/closetrycorner/controllers/UserController.java
@@ -1,8 +1,8 @@
 package com.codeup.closetrycorner.controllers;
 
+import org.springframework.stereotype.Controller;
 import com.codeup.closetrycorner.models.User;
 import com.codeup.closetrycorner.services.UserSvc;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -14,13 +14,18 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class UserController {
     private UserSvc userSvc;
 
-    public UserController(UserSvc users) {
-        this.userSvc = users;
+//    public UserController(UserSvc users) {
+//        this.userSvc = users;
+//    }
+
+    @GetMapping("/")
+    public String landing(){
+        return "index";
     }
 
     @GetMapping("/home")
     public String showHomepage() {
-        return "/index";
+        return "index";
     }
 
     @GetMapping("/register")
